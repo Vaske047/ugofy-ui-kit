2421409a3bd6bb7e3836677c00c2a6f275d930b0
aleksandar <vasic.aleksandar.bg@gmail.com>
2024-12-02 17:35:46 +0100

    Update exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
+export { default as theme } from './theme'
+export { default as ThemeProvider } from './theme/ThemeProvider'
+export { default as useTheme } from './theme/useTheme'
 export * from './components'
